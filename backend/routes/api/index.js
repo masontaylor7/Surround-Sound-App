4378d25adfa97b4127183c1e7812552322e1c810
masontaylor7 <masonevantaylor@gmail.com>
2022-03-03 15:11:37 -0800

    custom csrfFetch for CSRF in frontend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,9 +6,5 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 
-router.post('/test', function (req, res) {
-    res.json('this is inside of the /api/test router!')
-});
-
 
 module.exports = router;
