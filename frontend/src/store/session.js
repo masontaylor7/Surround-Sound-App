77afb45708572f0df88f3e9fba3288e79e23916f
masontaylor7 <masonevantaylor@gmail.com>
2022-03-03 22:38:02 -0800

    login form created and css added. need to resotre session user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,39 +1,51 @@
-import { csrfFetch } from "./csrf"
+import { csrfFetch } from './csrf';
 
-const SET_USER = 'session/SET_USER'
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
 
-const setUser = (user) => ({
-    type: SET_USER,
-    user
-})
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
 
 export const login = (user) => async (dispatch) => {
-    const { credential, password } = user
-    const response = await csrfFetch('/api/session/', {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ credential, password })
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
     });
-    if (response.ok) {
-        const data = await response.json();
-        console.log("this is the data", data)
-        dispatch(setUser(data.user));
-        return data;
-    }
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
-}
+const initialState = { user: null };
 
-
-const initialState = { user: null }
-
-const sessionReducer = ( state = initialState, action ) => {
+const sessionReducer = (state = initialState, action) => {
+    let newState;
     switch (action.type) {
-        case SET_USER: {
-            return {user: action.user};
-        }
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
         default:
             return state;
     }
-}
+};
 
 export default sessionReducer;
