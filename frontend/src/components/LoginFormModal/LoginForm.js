ee743b05c398f34df14f0da885cb0394c7d5a209
masontaylor7 <masonevantaylor@gmail.com>
2022-03-05 14:00:10 -0800

    login modal created

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,43 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import './LoginForm.css';
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
 
-function LoginFormPage() {
+function LoginForm() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector(state => state.session.user);
-    const [credential, setCredential] = useState('');
-    const [password, setPassword] = useState('');
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
-
-    if (sessionUser) return (
-        <Redirect to="/" />
-    );
-
-    // useEffect(() => {
-    //     let err = []
-    //     if (password.length < 1) {
-    //         err.push('Please enter a valid password')
-    //     }
-    //     setErrors(err)
-    // }, [password, credential])
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors([]);
-        return dispatch(sessionActions.login({ credential, password }))
-            .catch(async (res) => {
+        return dispatch(sessionActions.login({ credential, password })).catch(
+            async (res) => {
                 const data = await res.json();
                 if (data && data.errors) setErrors(data.errors);
-            });
+            }
+        );
     };
-
 
     return (
         <form onSubmit={handleSubmit}>
             <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                {errors.map((error, idx) => (
+                    <li key={idx}>{error}</li>
+                ))}
             </ul>
             <label>
                 Username or Email
@@ -62,4 +49,4 @@ function LoginFormPage() {
     );
 }
 
-export default LoginFormPage;
+export default LoginForm;
