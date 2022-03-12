d2e2e6aeff2576ff70fefa9daf5b3d95239d3734
masontaylor7 <masonevantaylor@gmail.com>
2022-03-11 19:02:49 -0800

    quick css fixes

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -4,11 +4,11 @@ import './HomePage.css';
 function HomePage() {
     return (
         <div className='main-body'>
-            <h1 className='welcome'>Welcome to Surround<span className='sound-text'>Sound</span>, where the world is your oyster...</h1>
+            <h1 className='welcome'>Welcome to Surround<span className='sound-text'>Sound</span>!</h1>
             <div className='list-block'>
-                <p>Upload your own <span>music</span>...</p>
-                <p>Create <span>playlists</span> to save your favorite tracks...</p>
-                <p>Find your new favorites!</p>
+                <p>A place for your own <span>creations</span>,</p>
+                <p>where you can ustomize <span>playlists</span> to <div>save your favorite tracks,</div></p>
+                <p>Discover new <span>music</span>!</p>
             </div>
 
             <img className='crowd-png' src='https://i.dlpng.com/static/png/4826358_preview_preview.png' alt='' />
